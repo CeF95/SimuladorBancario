023e436daa7c85e86ddf9d70c945b03ee85a33b1
CeF95 <celeste.fuentes.g30@gmail.com>
2026-05-01 05:37:34 -0600

    aplicacion de los metodos

--- src/com/banco/estructuras/Pila.java
+++ src/com/banco/estructuras/Pila.java
@@ -4,5 +4,25 @@ import com.banco.modelos.Cliente;
 import com.banco.modelos.Nodo;
 
 public class Pila {
-    
+    private Nodo tope;
+
+    public void apilar(Cliente c) {
+        Nodo nuevo = new Nodo(c);
+        nuevo.siguiente = tope;
+        tope = nuevo;
+    }
+
+    public Cliente verTope() {
+        return (tope != null) ? tope.cliente : null;
+    }
+
+    public void mostrar() {
+        Nodo temp = tope;
+        if (temp == null) 
+            System.out.println("La pila está vacía.");
+        while (temp != null) {
+            System.out.println(temp.cliente);
+            temp = temp.siguiente;
+        }
+    }
 }
