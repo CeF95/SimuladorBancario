a9f0eeea209d788211230015216e945028749cb0
CeF95 <celeste.fuentes.g30@gmail.com>
2026-05-01 05:31:32 -0600

    Clase cola

--- src/com/banco/estructuras/Cola.java
+++ src/com/banco/estructuras/Cola.java
@@ -1,5 +1,40 @@
 package com.banco.estructuras;
 
+import com.banco.modelos.Cliente;
+import com.banco.modelos.Nodo;
+
 public class Cola {
-    
+    private Nodo frente, finalCola;
+
+    public void encolar(Cliente c) {
+        Nodo nuevo = new Nodo(c);
+        if (frente == null) {
+            frente = nuevo;
+        } else {
+            finalCola.siguiente = nuevo;
+        }
+        finalCola = nuevo;
+    }
+
+    public Cliente desencolar() {
+        if (frente == null) return null;
+        Cliente c = frente.cliente;
+        frente = frente.siguiente;
+        if (frente == null) finalCola = null;
+        return c;
+    }
+        
+    public void mostrar() {
+        Nodo temp = frente;
+        if (temp == null) 
+            System.out.println("La cola está vacía.");
+        while (temp != null) {
+            System.out.println(temp.cliente);
+            temp = temp.siguiente;
+        }
+    }
+
+    public boolean estaVacia() {
+        return frente == null;
+    }
 }
