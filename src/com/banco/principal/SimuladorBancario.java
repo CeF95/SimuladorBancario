12d3bb0d07600bdcfa7626165c79b28af331d671
CeF95 <celeste.fuentes.g30@gmail.com>
2026-05-01 05:51:47 -0600

    menu

--- src/com/banco/principal/SimuladorBancario.java
+++ src/com/banco/principal/SimuladorBancario.java
@@ -7,5 +7,26 @@ import java.util.Scanner;
 import java.io.*;
 
 public class SimuladorBancario {
+    private static Cola colaClientes = new Cola();
+    private static Pila historialAtencion = new Pila();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int option;
+        do {
+            System.out.println("\n--- SIMULADOR DE COLA BANCARIA ---");
+            System.out.println("1. Cargar clientes desde archivo");
+            System.out.println("2. Agregar cliente manualmente");
+            System.out.println("3. Atender cliente (desencolar y aplicar)");
+            System.out.println("4. Mostrar cola de clientes");
+            System.out.println("5. Mostrar historial de atención");
+            System.out.println("6. Consultar ultimo atendido (tope de pila)");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            option = scanner.nextInt();
+            scanner.nextLine(); 
+
+
+    }
     
 }
